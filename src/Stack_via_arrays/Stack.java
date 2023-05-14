5b5d0c755b71c0f4e1f436d2d73beca130e89ac8
kylerasinger <kyle.rasinger@gmail.com>
2023-05-14 17:04:29 -0400

    Initial test, it works

--- src/Stack_via_arrays/Stack.java
+++ src/Stack_via_arrays/Stack.java
@@ -1,8 +1,13 @@
+package Stack_via_arrays;
 
 public class Stack {
 
+	//test
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		System.out.print("Test");
 
 	}
 
